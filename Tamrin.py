12646c9e276c96edc024333fb5db02b882532abc
milad safari <miladhajisafari72@gmail.com>
2023-10-27 22:26:13 +0330

    Tamrin 1 Operators
    چهار عمل اصلی به همراه پرانتز و توان

---
+++ Tamrin.py
@@ -0,0 +1,27 @@
+# tamrin 1
+num1 = 10
+num2 = 5
+print(num1 * num2)
+
+# tamrin 2
+num3 = 2
+num4 = 4
+print(num3 / num4)
+
+# tamrin 3
+num5 = 5
+num6 = 8
+print(num5 + num6)
+
+# tamrin 4
+num7 = 3
+num8 = 1
+print(num7 - num8)
+
+# tamrin 5
+num9 = 2
+num10 = 4
+print(num9 ** num10)
+
+# tamrin 6
+print((num1*num2) + (num3 + num4) - num5 / num6 ** num7)
